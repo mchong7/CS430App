40469abede50e5786c5fa1de9fa38460a3cf42d8
Matthew Chong <mchong7@hawaii.edu>
2017-10-17 10:28:23 -1000

    Resolved errors in layout of main activity.

--- app/src/main/java/mchong/cs430app/Main.java
+++ app/src/main/java/mchong/cs430app/Main.java
@@ -14,7 +14,7 @@ public class Main extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
